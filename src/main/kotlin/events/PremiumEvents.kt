cbf59d36f5d56715ce75f785ecdcd2cc3c08b0d8
Lucas Bruck <lukabruck@gmail.com>
2022-06-14 23:19:47 +1000

    EVENTS | Define our types, ensure we have a uniform way for querying the date

--- src/main/kotlin/events/PremiumEvents.kt
+++ src/main/kotlin/events/PremiumEvents.kt
@@ -1,2 +1,38 @@
 package events
 
+import java.time.LocalDate
+
+sealed class PremiumEvent {
+    abstract val contractId: String
+}
+
+fun PremiumEvent.date() =
+    when (this) {
+        is ContractCreatedEvent -> LocalDate.parse(this.startDate)
+        is PriceIncreasedEvent -> LocalDate.parse(this.atDate)
+        is PriceDecreasedEvent -> LocalDate.parse(this.atDate)
+        is ContractTerminatedEvent -> LocalDate.parse(this.terminationDate)
+    }
+
+data class ContractCreatedEvent(
+    override val contractId: String,
+    val premium: Long,
+    val startDate: String
+) : PremiumEvent()
+
+data class PriceIncreasedEvent(
+    override val contractId: String,
+    val premiumIncrease: Long,
+    val atDate: String
+) : PremiumEvent()
+
+data class PriceDecreasedEvent(
+    override val contractId: String,
+    val premiumReduction: Long,
+    val atDate: String
+) : PremiumEvent()
+
+data class ContractTerminatedEvent(
+    override val contractId: String,
+    val terminationDate: String
+) : PremiumEvent()
---
+++ src/test/kotlin/TestPremiumEvent.kt
@@ -0,0 +1,24 @@
+import events.*
+import io.kotest.data.forAll
+import io.kotest.data.headers
+import io.kotest.data.row
+import io.kotest.data.table
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+
+class TestPremiumEvent {
+
+    @Test
+    fun `will parse the date correctly`() {
+        table(
+            headers("PremiumEvent", "Date"),
+            row(ContractCreatedEvent(contractId = "1", premium = 100L, startDate = "2020-01-01"), LocalDate.of(2020, 1, 1)),
+            row(PriceIncreasedEvent(contractId = "1", premiumIncrease = 100L, atDate = "2020-01-01"), LocalDate.of(2020, 1, 1)),
+            row(PriceDecreasedEvent(contractId = "1", premiumReduction = 100L, atDate = "2020-01-01"), LocalDate.of(2020, 1, 1)),
+            row(ContractTerminatedEvent(contractId = "1", terminationDate = "2020-01-01"), LocalDate.of(2020, 1, 1))
+        ).forAll { event, expectedDate ->
+            event.date() shouldBe expectedDate
+        }
+    }
+}
